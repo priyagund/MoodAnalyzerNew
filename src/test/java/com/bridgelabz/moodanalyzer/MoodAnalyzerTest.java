8cb78a42753fec51001bd370aee0fb07c829d01d
priyagund <priyagund18@gmail.com>
2019-11-26 11:12:45 +0530

    ADD:analysing Sad mood and method return sad mood

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest 
 {
-    @Test
-    public void givenMessage_returnMood()
-    {
-       MoodAnalyzer moodAnalyzer= new MoodAnalyzer();
-       String mood=moodAnalyzer.analyzeMood("THIS IS SAD MOOD");
-        Assert.assertEquals("SAD",mood);
-    }
 
+    @Test
+    public void giveMessage_whenSad_shouldReturnSad()
+    {
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
+        String sadMood=moodAnalyzer.analyzeMood("THIS IS SAD MOOD");
+        Assert.assertEquals("SAD",sadMood);
+    }
 
 }
